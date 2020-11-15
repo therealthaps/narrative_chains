c660bda18548a9588d6b884563264dfb246b7d41
Rik Bose <rbose@Riks-MBP.hsd1.il.comcast.net>
2020-11-14 18:13:46 -0600

    added val.csv and testing code

--- example.py
+++ example.py
@@ -10,14 +10,21 @@ def parse_test_instance(story):
     alternatives = [story[6], story[7]]
     return [chains.ParsedStory(id, id, chains.nlp(" ".join(story[2:6]+[a])), *(sentences+[chains.nlp(a)])) for a in alternatives]
 
+def story_answer(story):
+    """Tells you the correct answer. Return (storyid, index). 1 for the first ending, 2 for the second ending"""
+    #obviously you can't use this information until you've chosen your answer!
+    return story.InputStoryId, story.AnswerRightEnding
 
 # Load training data
-data, table = chains.process_corpus("train.csv", 100)
-print(table.pmi("move", "nsubj", "move", "nsubj"))
+#data, table = chains.process_corpus("train.csv", 100)
+#print(table.pmi("move", "nsubj", "move", "nsubj"))
 
 # load testing data
-test = load_data("test.csv")
+test = chains.load_data("val.csv")
 for t in test:
     one, two = parse_test_instance(t)
-    one_deps = extract_dependency_pairs(one)
+    one_deps = chains.extract_dependency_pairs(one)
+    print(one[2:])
+    print(two[2:])
     # logic to choose between one and two
+    print("answer:", story_answer(t))
